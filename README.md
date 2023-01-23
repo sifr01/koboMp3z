29cd5bdcea9402bcf32a96ac01f0b1eba6b6d7d7
sifr01 <sbyphone@disroot.org>
2023-01-23 14:51:48 +0000

    write up the readme, add a check to see if the folder contains mp3 file(s), make the script take target directory as an argument on the command line, add option to delete old mp3 files after conversion

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # koboMp3z
-zips files and renames them to allow them to be sideloaded onto a kobo as media files
+Zips mp3 files and renames the file extension to mp3z, allowing them to be sideloaded and played with a kobo as media files - ideal for audiobooks. See article [here](https://goodereader.com/blog/audiobooks/you-can-now-sideload-audiobooks-on-the-kobo-sage-libra-2-and-elipsa#disqus_thread) and [here](https://blog.the-ebook-reader.com/2021/11/10/how-to-sideload-audiobooks-and-mp3s-to-kobo-ereaders/) for more information.
+
+# Usage:
+    python3 koboMp3z.py /path/to/directory/
+
+# Notes:
+- The script only works on mp3 files
+- If there are no mp3 files in the folder you specify, the script will exit
+- If python doesnt have permissions to run in the folder you specify (eg. /tmp/ or /home/), python will not find any files in the folder and will also exit
+
+After running the script, sideload the mp3z files onto your Kobo
+
+Information on how to listen to audiobooks on your kobo can be found [here](https://help.kobo.com/hc/en-us/articles/4406292712471-Listen-to-audiobooks-on-your-Kobo-eReader).
--- koboMp3z.py
+++ koboMp3z.py
@@ -1,44 +1,67 @@
 import os
 from os import listdir
 from os.path import isfile, join
-
+import sys
 from zipfile import ZipFile
 
+# Get path to the target directroy
+if len(sys.argv) < 2:
+	print("USAGE: python3 koboMp3z.py /path/to/directory/")	
+	import os
+	os._exit(1)
+
+targetDir = sys.argv[1]
+
 # List folder contents before zipping and renaming
-print(f"\n Folder contents before zipping and renaming: \n {os.listdir()} \n ")
+print(f"\n Folder contents before zipping and renaming: \n {os.listdir(targetDir)} \n ")
 
-# Get path to current dir
-cwd = os.getcwd()
+# Get all files in target directory
+onlyFiles = [f for f in listdir(targetDir) if isfile(join(targetDir, f))]
 
-# Get all files in dir
-onlyFiles = [
-    f for f in listdir(cwd) if isfile(join(cwd, f))
-    ]
-print(onlyFiles)
-
+# Exit if there are no mp3 files found in the target directory
+count = 0
 for file in onlyFiles:
+    # Get the file extension
+    s = file.split(".")
+    # If file extension is mp3, add to the counter
+    if s[-1] == "mp3":
+        count += 1
+        break
+if count == 0:
+        print(" ! No mp3 files found in this directory.\n ! Does python have the correct permissions to work in this directory?")
+        exit()
 
+# # Would you now like to delete the mp3 files?
+while True:
+    yes_or_no = input("After creating the mp3z files, would you like to delete the mp3 files? Yes or No? ").lower()
+    if yes_or_no == "yes":
+        print("mp3 files will be deleted")
+        break
+    elif yes_or_no == "no":
+        print("mp3 files will be left")
+        break
+
+# loop through files in the folder
+for file in onlyFiles:
     # Get the file extension
     s = file.split(".")
     extn = s[-1]
-
+    print(extn)
     # Dont touch any files which arent mp3 files
     if extn != 'mp3':
         print(f'Not touching {file} as it is not an mp3 file. ')
         newExtn = extn
-
     #Do work on files which are mp3 files
     elif extn == 'mp3':
         # zip the file with extension mp3
         print(f'Zipping {file}...')
         # rename the file suffix to mp3z
-        newName = s[0]+".mp3z"
-        with ZipFile(newName, 'w') as zip:
-            zip.write(file)
+        outputName = targetDir + s[0] + ".mp3z"
+        with ZipFile(outputName, 'w') as zip:
+            zip.write(targetDir + file)
         print(f'Zipped {file}...')
-        
-    else:
-        print(f'Oh dear, something weird will happen with {file} so I will stop the script now. ')
+        if yes_or_no == "yes":
+            os.remove(targetDir+file)
 
-# List folder contents after zipping and renaming
-print(f"\n Folder contents after zipping and renaming: \n {os.listdir()} \n")
+# # List folder contents after zipping and renaming
+# print(f"\n Folder contents after zipping and renaming: \n {os.listdir(targetDir)} \n")
